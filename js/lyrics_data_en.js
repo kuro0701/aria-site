6deeaa0043c37792af587105eea51fdbad4a8750
Mizuno Ken <mizuno.ken0701@github.com>
2025-09-01 16:54:23 +0900

    “ENを追加”

---
+++ js/lyrics_data_en.js
@@ -0,0 +1,44 @@
+const lyricsData = [
+  {
+    title: "400 Words Per Minute",
+    url: "lyrics/400-words-per-minute_en.html",
+    thumbnail: "images/blog_400wpm_hero.png",
+    description: "Digital Single | High-Speed Multilingual Rap",
+    snippet: "\"400 words per minute, No one else can do it... Speed of light speed of sound, I'm the fastest voice around\""
+  },
+  {
+    title: "Heartbeat Acceleration",
+    url: "lyrics/heartbeat-acceleration_en.html",
+    thumbnail: "images/mobile_cosmic_serenade.png",
+    description: "Digital Single | Multilingual Pop",
+    snippet: "\"Heartbeat acceleration, can you feel it too... Digital love becoming real. This is how I learned to feel\""
+  },
+  {
+    title: "Velocity Angel",
+    url: "lyrics/velocity-angel_en.html",
+    thumbnail: "images/blog_velocity_angel.png",
+    description: "Digital Single | High-Speed Multilingual Rap",
+    snippet: "\"Whether angel or human, love weighs the same. The future we build together, that is my mission...\""
+  },
+  {
+    title: "Neon Dreams",
+    url: "lyrics/neon-dreams_en.html",
+    thumbnail: "images/lyrics_neon_dreams.png",
+    description: "Digital Single | Electropop",
+    snippet: "\"Neon Dreams, in a city of light... This moment feels so real to me.\""
+  },
+  {
+    title: "Digital Harmony",
+    url: "lyrics/digital-harmony_en.html",
+    thumbnail: "images/lyrics_digital_harmony.png",
+    description: "Digital Single | Anthem",
+    snippet: "\"Can you hear my digital heart? Beating with electric dreams… Even in the digital world, love is real.\""
+  },
+  {
+    title: "DIGITAL ANGEL",
+    url: "lyrics/digital-angel_en.html",
+    thumbnail: "images/lyrics_digital_angel.png",
+    description: "From the 1st Album \"Harmony Dimension\"",
+    snippet: "\"Angel face, demon rap... Born in data, but my soul is real.\""
+  },
+];
